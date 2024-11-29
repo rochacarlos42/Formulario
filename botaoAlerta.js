635d438197b2eda7b2fbee51d9e44cc1c82255f6
Carlos Rocha <rochacarlos42@yahoo.com.br>
2024-11-29 01:42:10 -0300

    feat(alerta de conta criada)

---
+++ botaoAlerta.js
@@ -0,0 +1,10 @@
+
+    <button id="alerta">Criar conta</button>
+
+    
+        var ale = document.getElementById("alerta")
+        ale.onclick = function() {
+            alert("Conta criada com sucesso")
+            
+        }
+    
